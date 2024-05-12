66c2fd46d965a17a6196d517468464af8581d8ac
Ampoi <tange.toshihiro@outlook.jp>
2024-05-12 10:07:41 +0900

    円の形を見出せた

--- src/App.vue
+++ src/App.vue
@@ -13,12 +13,14 @@
                     tick
                 </button>
             </div>
+            <div ref="canvas"/>
         </div>
     </main>
 </template>
 <script setup lang="ts">
-import { computed, ref } from "vue";
+import { computed, onMounted, ref } from "vue";
 import { DiagonalLineAlgorithm } from "./algorithms/main";
+import p5 from "p5";
 
 const pi = ref(0);
 const episode = ref(0);
@@ -31,4 +33,96 @@ const tick = () => {
         episode.value = newEpisode;
     })
 }
+
+const canvas = ref<HTMLElement>()
+
+const a = (p: p5) => {
+    p.setup = () => {
+        p.createCanvas(400, 400);
+        p.colorMode(p.HSL);
+
+        function isInCircle([x, y]: [number, number]){
+            return (x ** 2 + y ** 2) <= 1
+        }
+        
+        function c(from: [number,number], direction: [0|1|-1, 0|1|-1], depth: number){
+            const size = 200
+            const dVectorSize = 2 ** depth
+            const to: [number, number] = [(from[0] + direction[0] / dVectorSize), (from[1] + direction[1] / dVectorSize)]
+
+            const isFromInCircle = isInCircle(from)
+            const isToInCircle = isInCircle(to)
+            const position = isFromInCircle && isToInCircle
+                ? "in"
+                : isFromInCircle || isToInCircle
+                    ? "cross"
+                    : "out"
+
+            p.stroke(
+                position == "cross"
+                    ? p.color(depth * 20 % 360, 100, 50)
+                    : position == "in"
+                        ? p.color(0)
+                        : p.color(200)
+            )
+            p.line(
+                from[0] * size, from[1] * size,
+                to[0] * size, to[1] * size
+            )
+
+            if( direction[0] == 0 && direction[1] == 0 ) throw new Error("Direction is zero")
+            if( depth == 10 ) return
+
+            const newDepth = (direction[0] == 0 || direction[1] == 0) ? depth + 1 : depth
+            if( direction[0] == 1 ){
+                if( direction[1] == 1 ){
+                    //↗︎
+                    c(to, [-1, 0], newDepth)
+                    c(to, [0, -1], newDepth)
+                }else if( direction[1] == 0 ){
+                    //→
+                    c(to, [-1, 1], newDepth)
+                    c(to, [-1, -1], newDepth)
+                }else{
+                    //↘︎
+                    c(to, [0, 1], newDepth)
+                    c(to, [-1, 0], newDepth)
+                }
+            }else if( direction[0] == 0 ){
+                if( direction[1] == 1 ){
+                    //↑
+                    c(to, [1, -1], newDepth)
+                    c(to, [-1, -1], newDepth)
+                }else{
+                    //↓
+                    c(to, [1, 1], newDepth)
+                    c(to, [-1, 1], newDepth)
+                }
+            }else{
+                if( direction[1] == 1 ){
+                    //↖︎
+                    c(to, [1, 0], newDepth)
+                    c(to, [0, -1], newDepth)
+                }else if( direction[1] == 0 ){
+                    //←
+                    c(to, [1, 1], newDepth)
+                    c(to, [1, -1], newDepth)
+                }else{
+                    //↙︎
+                    c(to, [0, 1], newDepth)
+                    c(to, [1, 0], newDepth)
+                }
+            }
+        }
+
+        p.strokeWeight(2)
+        
+        c([0.5,0.5],[1,0],1)
+    }
+}
+
+onMounted(() => {
+    if(!canvas.value) throw new Error("Canvas not found")
+    new p5(a, canvas.value)
+})
 </script>
